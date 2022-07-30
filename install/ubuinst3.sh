6ec5dcf262c90046a511c08acdab6070c2fb386d
PlaYCoNecT <andin.cell.1987@gmail.com>
2022-07-30 13:31:54 -0300

    Update ubuinst3.sh

--- install/ubuinst3.sh
+++ install/ubuinst3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat /dev/null > ~/.bash_history && history -c
 rm /bin/ubuinst* > /dev/null 2>&1
-echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3BsYXljb25lY3QvUGFpbmVsLTIwMjIvbWFpbi9pbnN0YWxsL3VidWluc3QyLnNoICYmIGNobW9kICt4IHVidWluc3QyLnNoICYmIGRvczJ1bml4IHVidWluc3QyLnNoID4gL2Rldi9udWxsIDI+JjEKY2QgfHwgZXhpdA==' | base64 -d | bash
+echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3BsYXljb25lY3QvUGFpbmVsNGctMjAyMi9tYWluL2luc3RhbGwvdWJ1aW5zdDIuc2ggJiYgY2htb2QgK3ggdWJ1aW5zdDIuc2ggJiYgZG9zMnVuaXggdWJ1aW5zdDIuc2ggPiAvZGV2L251bGwgMj4mMQpjZCB8fCBleGl0' | base64 -d | bash
 /bin/ubuinst2.sh
